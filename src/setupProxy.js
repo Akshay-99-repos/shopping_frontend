1b9dd8ba1fefca48496e1d296a1526671cd55a5a
unknown <cogjava2542@iiht.tech>
2022-10-25 14:35:47 +0530

    Added AWS URLs

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/app1', {
-      target: 'http://localhost:8082', // API endpoint 1
-    //   target: 'http://3.87.108.193:8082',
+      target: 'http://shoppingappuserservice-env.eba-x22nuz2r.us-east-2.elasticbeanstalk.com', // API endpoint 1
+    //   target: 'http://localhost:8082',
       changeOrigin: true,
       pathRewrite: {
         "^/app1": "",
@@ -16,8 +16,8 @@ module.exports = function (app) {
   );
   app.use(
     createProxyMiddleware('/app2', {
-      target: 'http://localhost:8083', // API endpoint 2
-    //   target: 'http://34.229.167.216:8083',
+      target: 'http://shoppingappproductservice-env.eba-2sppmnhk.us-east-2.elasticbeanstalk.com', // API endpoint 2
+    //   target: 'http://localhost:8083',
       changeOrigin: true,
       pathRewrite: {
         "^/app2": "",
